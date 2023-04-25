17271073e993fe3d5a7671b8ec4c17a593e9ee7e
Alexey Koloty <rico777@gmail.com>
2023-04-25 14:03:02 +0300

    Extend APIs

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,8 @@ namespace API.Extensions
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepostory>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             return services;
         }
     }
