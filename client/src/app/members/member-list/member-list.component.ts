8a77f27be9dd91703581f45a10409862cac12720
Alexey Koloty <rico777@gmail.com>
2023-05-10 09:24:39 +0300

    Some optimization

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Member } from 'src/app/models/member.interface';
+import { Pagination } from 'src/app/models/pagination';
+import { User } from 'src/app/models/user.model';
+import { UserParams } from 'src/app/models/userParams';
+import { AccountService } from 'src/app/services/account.service';
 import { MembersService } from 'src/app/services/members.service';
 
 @Component({
@@ -10,13 +14,43 @@ import { MembersService } from 'src/app/services/members.service';
 })
 export class MemberListComponent implements OnInit {
 
-  members$: Observable<Member[]> | undefined;
+  //members$: Observable<Member[]> | undefined;
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  userParams: UserParams | undefined;
+  user: User | undefined; // Do we use it?
 
   constructor(
-    private membersService: MembersService
-  ){}
+    private membersService: MembersService,
+    private accountService: AccountService
+  ){
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      if(user){
+        this.userParams = new UserParams(user);
+        this.user = user;
+      }
+    });
+  }
 
   ngOnInit(): void {
-    this.members$ = this.membersService.getMembers();
+    //this.members$ = this.membersService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers(){
+    if(!this.userParams) return;
+    this.membersService.getMembers(this.userParams).subscribe(response => {
+      if(response.result && response.pagination){
+        this.members = response.result;
+        this.pagination = response.pagination;
+      }
+    })
+  }
+
+  pageChanged(event: any){
+    if(this.userParams && this.userParams.pageNumber !== event.page){
+      this.userParams.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member.interface';
 import { map, of, tap } from 'rxjs';
+import { PaginatedResult } from '../models/pagination';
+import { UserParams } from '../models/userParams';
 
 @Injectable({
   providedIn: 'root'
@@ -16,12 +18,38 @@ export class MembersService {
     private http: HttpClient
   ) { }
 
-  getMembers()
-  {
-    if(this.members.length > 0) return of(this.members);
-    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      tap(members => this.members = members)
+  getMembers(userParams: UserParams){
+
+    let params = this.getPaginationHeaders(userParams);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params);
+  }
+
+  private getPaginatedResult<T>(url: string, params: HttpParams) {
+    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>;
+    return this.http.get<T>(url, { observe: 'response', params }).pipe(
+      map(response => {
+        if (response.body) {
+          paginatedResult.result = response.body;
+        }
+        const pagination = response.headers.get('Pagination');
+        if (pagination) {
+          paginatedResult.pagination = JSON.parse(pagination);
+        }
+        return paginatedResult;
+      })
     );
+  }
+
+  private getPaginationHeaders(userParams: UserParams) {
+    let params = new HttpParams();
+    params = params.append('pageNumber', userParams.pageNumber);
+    params = params.append('pageSize', userParams.pageSize);
+    params = params.append('minAge', userParams.minAge);
+    params = params.append('maxAge', userParams.maxAge);
+    params = params.append('gender', userParams.gender);
+
+    return params;
   }
 
   getMember(username: string)
