675946fbb7fd7182443a650bb47998fd12782c62
Alexey Koloty <rico777@gmail.com>
2023-05-28 14:47:45 +0300

    SignalR

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -11,10 +11,9 @@ import { MessageService } from 'src/app/services/message.service';
 export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
-  @Input() messages: Message[] = [];
   messageContent = '';
 
-  constructor(private messageService: MessageService){}
+  constructor(public messageService: MessageService){}
 
   ngOnInit(): void {
 
@@ -23,9 +22,8 @@ export class MemberMessagesComponent implements OnInit {
   sentMessage(){
     if(!this.username) return;
 
-    this.messageService.sendMessage(this.username, this.messageContent).subscribe(message => {
-      this.messages.push(message);
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm?.reset();
-    })
+    });
   }
 }
